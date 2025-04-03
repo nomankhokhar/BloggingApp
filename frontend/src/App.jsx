186764e04fd7606cb0c243ea0f6c8b027e78af59
nomannhohar <aliyounas084@gmail.com>
2025-04-03 22:43:35 +0500

    added starter code of blogging website

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <div className="px-4 md:px-8 lg:px16 lx:px-32 2xl:px-64">
+      {/* Navbar */}
+      <Navbar />
+      {/* BREADCURM */}
+      {/* INTRODUCTION */}
+      {/* FEATURED POSTS */}
+      {/* POSY LIST */}
+    </div>
+  );
+};
+
+export default App;
